6310828bf1eeae98385809ed414b54dd53d43da7
Yuliya Mukhina <karin.panch@gmail.com>
2024-03-27 11:41:21 +0300

    App and Table components, without functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/main.css';
+
+var data = [
+  {id: 1, name: 'Gob', value: '2'},
+  {id: 2, name: 'Buster', value: '5'},
+  {id: 3, name: 'George Michael', value: '4'}
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header >
+        Hello World! Привет, мир!
       </header>
+      <p className="Table-header">Basic Table</p> 
+      <Table1 data={data}/>
     </div>
   );
 }
